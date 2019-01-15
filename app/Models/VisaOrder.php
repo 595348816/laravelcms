6bfeadfb9ff63d3feb946013ad45595fe0599dfa
zxp_lethe <zxp_lethe@163.com>
2019-01-15 17:57:02 +0800

    定义订单模型金额修改器和获取器

--- app/Models/VisaOrder.php
+++ app/Models/VisaOrder.php
@@ -9,4 +9,18 @@ class VisaOrder extends Model
     protected $fillable = [
         'type', 'money', 'remark','pay_time'
     ];
+
+    /**
+     * 支付金额入库*100 单位分
+     * @param $value 支付金额
+     */
+    public function setMoneyAttribute($value)
+    {
+        $this->attributes['money']=$value*100;
+    }
+
+    public function getMoneyAttribute($value)
+    {
+        return $value/100;
+    }
 }
